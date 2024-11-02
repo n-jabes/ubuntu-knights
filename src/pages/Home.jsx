c8dbbddfcf598ff66c2839e1b55ae4c7766e695e
n-jabes <nshutij7@gmail.com>
2024-11-02 20:25:23 +0200

    feature: added floating island and assets

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Canvas } from '@react-three/fiber';
 import React, { Suspense } from 'react';
 import Loader from '../components/Loader';
+import Island from '../models/Island';
 
 {
   /* <div className="absolute top-28 right-0 left-0 z-10 flex items-center justify-center">
@@ -8,13 +9,39 @@ import Loader from '../components/Loader';
 </div> */
 }
 const Home = () => {
+  const adjustIslandForScreenSize = () => {
+    let screenScale = null;
+    let screenPostion = [0, -6.5, -43];
+    let rotation = [0.1, 4.7, 0];
+
+    if (window.innerWidth < 768) {
+      screenScale = [0.9, 0.9, 0.9];
+    } else {
+      screenScale = [1, 1, 1];
+    }
+
+    return [screenScale, screenPostion, rotation];
+  };
+
+  const [islandScale, islandPosition, islandRotation] =
+    adjustIslandForScreenSize();
   return (
     <section className="w-full h-screen relative">
       <Canvas
         className="w-full h-screen bg-transparent"
         camera={{ near: 0.1, far: 1000 }}
       >
-        <Suspense fallback={<Loader />}></Suspense>
+        <Suspense fallback={<Loader />}>
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.2}/>
+          <hemisphereLight skyColor='#b1e1ff' groundColor='#000000' intensity={0.5}/>
+
+          <Island
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+          />
+        </Suspense>
       </Canvas>
     </section>
   );
