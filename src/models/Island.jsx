002dc4ffe9cafa60cd673dd80218da7e843a606f
n-jabes <nshutij7@gmail.com>
2024-11-03 21:00:13 +0200

    update: rotate the sky

--- src/models/Island.jsx
+++ src/models/Island.jsx
@@ -13,16 +13,13 @@ import { a } from '@react-spring/three';
 
 import islandScene from '../assets/3d/island.glb';
 
-const Island = ({ isRotating, setIsRotating, ...props }) => {
+const Island = ({ isRotating, setIsRotating, setCurrentStage, ...props }) => {
   const islandRef = useRef();
   const { gl, viewport } = useThree();
   const { nodes, materials } = useGLTF(islandScene);
   const lastX = useRef(0);
   const rotationSpeed = useRef(0);
   const dampingFactor = 0.95;
-
-  // Add a state for tracking the current stage
-  const [currentStage, setCurrentStage] = useState(null);
 
   const handlePointerDown = (e) => {
     e.stopPropagation();
--- src/models/Plane.jsx
+++ src/models/Plane.jsx
@@ -1,14 +1,25 @@
-import React from 'react'
-import planeScene from '../assets/3d/plane.glb'
-import { useGLTF } from '@react-three/drei'
+import React, { useEffect, useRef } from 'react';
+import planeScene from '../assets/3d/plane.glb';
+import { useAnimations, useGLTF } from '@react-three/drei';
 
-const Plane = ({isRotating, ...props}) => {
-    const {scene, animations } = useGLTF(planeScene)
+const Plane = ({ isRotating, ...props }) => {
+  const ref = useRef();
+  const { scene, animations } = useGLTF(planeScene);
+  const { actions } = useAnimations(animations, ref);
+
+  useEffect(() => {
+    if (isRotating) {
+      actions['Take 001'].play();
+    } else {
+      actions['Take 001'].stop();
+    }
+  }, [actions, isRotating]);
+
   return (
-    <mesh {...props}>
-        <primitive object={scene} />
+    <mesh ref={ref} {...props}>
+      <primitive object={scene} />
     </mesh>
-  )
-}
+  );
+};
 
-export default Plane
+export default Plane;
