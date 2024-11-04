2a1a1033bacb78d72cb86f4bcaab22cfe0a848c9
n-jabes <nshutij7@gmail.com>
2024-11-04 16:11:34 +0200

    feature: add the contact us form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,9 +1,109 @@
-import React from 'react'
+import React, { useRef, useState } from 'react';
+import emailjs from '@emailjs/browser';
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const formRef = useRef(null);
+  const [form, setForm] = useState({ name: '', email: '', message: '' });
+  const [isLoading, setIsLoading] = useState(false);
 
-export default Contact
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleFocus = () => {};
+  const handleBlur = () => {};
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsLoading(true);
+    emailjs
+      .send(
+        import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+        import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+        {
+          from_name: form.name,
+          to_name: 'Ubuntu Knights Think Tank',
+          from_email: form.email,
+          to_email: 'nshutij7@gmail.com',
+          message: form.message,
+        },
+        import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+      )
+      .then(() => {
+        setIsLoading(false);
+        //TODO: Show success message
+        //TODO: Hide an alert
+        setForm({ name: '', email: '', message: '' });
+      })
+      .catch((error) => {
+        setIsLoading(false);
+        console.log(error);
+        //TODO: Show error message
+      });
+  };
+
+  return (
+    <section className="flex lg:flex-row flex-col max-container">
+      <div className="flex-1 min-w-[50%] flex flex-col">
+        <h1 className="head-text">Get in touch</h1>
+        <form
+          action=""
+          ref={formRef}
+          onSubmit={handleSubmit}
+          className="w-full flex flex-col gap-7 mt-14"
+        >
+          <label htmlFor="" className="text-black-500 font-semibold">
+            Name
+            <input
+              type="text"
+              name="name"
+              className="input"
+              placeholder="John Doe"
+              required
+              value={form.name}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label htmlFor="" className="text-black-500 font-semibold">
+            Email
+            <input
+              type="email"
+              name="email"
+              className="input"
+              placeholder="johndoe@gmail.com"
+              required
+              value={form.email}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+          <label htmlFor="" className="text-black-500 font-semibold">
+            Your Message
+            <textarea
+              name="message"
+              className="textarea"
+              placeholder="We appreciate your feedback!"
+              required
+              value={form.message}
+              onChange={handleChange}
+              onFocus={handleFocus}
+              onBlur={handleBlur}
+            />
+          </label>
+
+          <button
+            type="submit"
+            className="btn"
+            onFocus={handleFocus}
+            onBlur={handleBlur}
+          >
+            {isLoading ? 'Sending...' : 'Send Message'}
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Contact;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,6 @@
 import Home from './Home'
 import About from './About'
 import Challenges from './Challenges'
-import Contact from './Home'
+import Contact from './Contact'
 
 export {Home, About, Challenges, Contact};
