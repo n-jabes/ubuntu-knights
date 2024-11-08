96f6cbf894e7fb7c4ae0bf3395368eaae8f1fef2
n-jabes <nshutij7@gmail.com>
2024-11-08 10:03:52 +0200

    feature: add the projects page

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import {Home, About, Challenges, Contact } from './pages'
+import { Home, About, Challenges, Contact } from './pages';
 
 const App = () => {
   return (
-    <main className="bg-slate-300/20 h-[100vh]">
+    <main className="bg-slate-300/20 h-full">
       <Router>
         <Navbar />
         <Routes>
-          <Route path="/" element={<Home/>} />
-          <Route path="/about" element={<About/>} />
-          <Route path="/challenges" element={<Challenges/>} />
-          <Route path="/contact" element={<Contact/>} />
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/challenges" element={<Challenges />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </Router>
     </main>
